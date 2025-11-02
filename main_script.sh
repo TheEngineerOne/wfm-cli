361495d374abf390cba5eb2a6884482f4dd7c09d
theengineerone <verbruggenlucas2@gmail.com>
2025-11-02 14:09:10 +0100

    Adding buffered way to pull data

--- main_script.sh
+++ main_script.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 base_url="https://api.warframe.market/v2"
+prerequest_buffer=""
+
 search_slug() {
   curl -s "$base_url/items" | jq -r '.data[] | "\(.slug)\t\(.i18n.en.name)"' | fzf --with-nth=2 --delimiter='\t' | cut -f1
 }
@@ -21,4 +23,13 @@ get_market_size_from_slug() {
   get_ingame_sell_from_slug "$1" | jq -r '.quantity' | awk '{sum+=$1} END {print sum}'
 }
 
-get_market_size_from_slug "$(search_slug)"
+buffered_ingame_sell() {
+  echo "$1" | jq -r '.data[] | select(.type == "sell")' | jq -r 'select(.user.status=="ingame")'
+}
+
+buffered_market_size() {
+  buffered_ingame_sell "$1" | jq -r '.quantity' | awk '{sum+=$1} END {print sum}'
+}
+
+prerequest_buffer="$(get_orders_from_slug "$(search_slug)")"
+volume="$(buffered_market_size "$prerequest_buffer")"
