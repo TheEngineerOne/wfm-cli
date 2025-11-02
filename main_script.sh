9ff1bb673c793deac2ce029fbab14edbef961c2b
theengineerone <verbruggenlucas2@gmail.com>
2025-11-02 13:41:44 +0100

    Adding base script to fetch info about items from slugs

---
+++ main_script.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+base_url="https://api.warframe.market/v2"
+search_slug() {
+  curl -s "$base_url/items" | jq -r '.data[] | "\(.slug)\t\(.i18n.en.name)"' | fzf --with-nth=2 --delimiter='\t' | cut -f1
+}
+
+get_info_from_slug() {
+  curl -s "$base_url/item/$1" | jq
+}
+
+get_info_from_slug "$(search_slug)"
